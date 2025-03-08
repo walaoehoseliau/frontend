3420f5246bd54c06b01ceed031ecad5e0037bed2
walaoehoseliau <199198331+walaoehoseliau@users.noreply.github.com>
2025-03-09 06:29:12 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -97,7 +97,7 @@ function App() {
           onChange={(e) => setKeyword(e.target.value)}
           maxLength={100}
           style={{
-            width: "93%",
+            width: "90%",
             padding: "12px",
             fontSize: "16px",
             marginBottom: "12px",
